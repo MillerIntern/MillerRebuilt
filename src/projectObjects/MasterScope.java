885f05b5e91ab4ec0c2761b38fd68425e0f7b6c8
Lillian Nicholas <lanicholas@loyola.edu>
2019-04-25 14:33:03 -0400

    Add files via upload

---
+++ src/projectObjects/MasterScope.java
@@ -0,0 +1,80 @@
+package projectObjects;
+
+public class MasterScope {
+	
+	
+	private int projectItem;
+	private String item1;
+	private String item2;
+	private String item3;
+	private String item4;
+	private String item5;
+	private String item6;
+	private String item7;
+	private String item8;
+	private String item9;
+	private String item10;
+	private int quantity1;
+	private int quantity2;
+	private int quantity3;
+	private int quantity4;
+	private int quantity5;
+	private int quantity6;
+	private int quantity7;
+	private int quantity8;
+	private int quantity9;
+	private int quantity10;
+
+	
+	public MasterScope(int projItem, String item1, String item2, String item3, String item4, String item5, String item6, 
+			           String item7, String item8, String item9, String item10, int quantity1, int quantity2, int quantity3,
+			           int quantity4, int quantity5, int quantity6, int quantity7, int quantity8, int quantity9, int quantity10)
+	{
+	   	this.projectItem = projItem;
+	   	this.item1 = item1;
+	   	this.item2 = item2;
+	   	this.item3 = item3;
+	   	this.item4 = item4;
+	   	this.item5 = item5;
+	   	this.item6 = item6;
+	   	this.item7 = item7;
+	   	this.item8 = item8;
+	   	this.item9 = item9;
+	   	this.item10 = item10;
+	   	this.quantity1 = quantity1;
+	   	this.quantity2 = quantity2;
+	   	this.quantity3 = quantity3;
+	   	this.quantity4 = quantity4;
+	   	this.quantity5 = quantity5;
+	   	this.quantity6 = quantity6;
+	   	this.quantity7 = quantity7;
+	   	this.quantity8 = quantity8;
+	   	this.quantity9 = quantity9;
+	   	this.quantity10 = quantity10;
+	}
+	
+	public MasterScope()
+	{
+	   	this.projectItem = 0;
+	   	this.item1 = null;
+	   	this.item2 = null;
+	   	this.item3 = null;
+	   	this.item4 = null;
+	   	this.item5 = null;
+	   	this.item6 = null;
+	   	this.item7 = null;
+	   	this.item8 = null;
+	   	this.item9 = null;
+	   	this.item10 = null;
+	   	this.quantity1 = 0;
+	   	this.quantity2 = 0;
+	   	this.quantity3 = 0;
+	   	this.quantity4 = 0;
+	   	this.quantity5 = 0;
+	   	this.quantity6 = 0;
+	   	this.quantity7 = 0;
+	   	this.quantity8 = 0;
+	   	this.quantity9 = 0;
+	   	this.quantity10 = 0;	
+	}
+}
