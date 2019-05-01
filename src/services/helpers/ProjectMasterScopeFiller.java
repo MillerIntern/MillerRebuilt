d203727aa3fe678fc1ee12d1cfbca1e59985e08e
Lillian Nicholas <lanicholas@loyola.edu>
2019-05-01 11:47:34 -0400

    Add files via upload

---
+++ src/services/helpers/ProjectMasterScopeFiller.java
@@ -0,0 +1,14 @@
+package services.helpers;
+
+import java.text.ParseException;
+import java.util.Map;
+
+import projectObjects.ProjectMasterScope;
+
+public class ProjectMasterScopeFiller {
+	
+	public synchronized static void fillProjectMasterScope(ProjectMasterScope pms,  Map<String, String>params) throws ParseException
+	{
+
+	}
+}
